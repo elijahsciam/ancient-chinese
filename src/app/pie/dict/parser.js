8c2a351ed7a12a01ea4e87c09238113a86079203
Elijah Sciammas <eli.sciammas@gmail.com>
2021-04-28 16:08:50 -0400

    Feat: using node.js file-system to properly read dictionary file, beginning parsing logic

--- src/app/pie/dict/parser.js
+++ src/app/pie/dict/parser.js
@@ -1,2 +1,18 @@
-const dict = require('./dict.txt');
-console.log(dict.toString());
+/* eslint-disable no-unused-vars */
+const fs = require('fs');
+
+const dictionary = fs.readFile(
+  'src/app/pie/dict/cedict_ts.u8',
+  'utf8',
+  (err, data) => {
+    if (err) throw err;
+    readData(data);
+  }
+);
+
+const readData = (data) => {
+  const lines = data.split('\n').map((line) => {
+    const word = line.split('/');
+    console.log(word[0]);
+  });
+};
